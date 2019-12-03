dc58286d300d0a21ba806a243d08bcbbfb4a77a1
Per Holmäng <per.holmang@gmail.com>
2019-12-03 21:44:43 +0100

    nicer

--- test_d3.py
+++ test_d3.py
@@ -1,4 +1,4 @@
-from d3 import build_grid, build_wires, closest_intersection
+from d3 import build_wire_grid, build_wires, closest_intersection
 
 
 def test_build_wires():
@@ -8,8 +8,8 @@ def test_build_wires():
 
 
 def test_build_grid():
-    assert build_grid("U1,R1,D1,L1") == [(0, 0), (0, 1), (1, 1), (1, 0)]
-    assert build_grid("U2,R2,D2,L2") == [
+    assert build_wire_grid("U1,R1,D1,L1") == [(0, 0), (0, 1), (1, 1), (1, 0)]
+    assert build_wire_grid("U2,R2,D2,L2") == [
         (0, 0),
         (0, 1),
         (0, 2),
@@ -19,7 +19,7 @@ def test_build_grid():
         (2, 0),
         (1, 0),
     ]
-    assert build_grid("R2,U2,L2,D2") == [
+    assert build_wire_grid("R2,U2,L2,D2") == [
         (0, 0),
         (1, 0),
         (2, 0),
@@ -34,15 +34,15 @@ def test_build_grid():
 def test_get_closest_intersection():
     assert (
         closest_intersection(
-            build_grid("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
-            build_grid("U62,R66,U55,R34,D71,R55,D58,R83"),
+            build_wire_grid("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+            build_wire_grid("U62,R66,U55,R34,D71,R55,D58,R83"),
         )
         == 159
     )
     assert (
         closest_intersection(
-            build_grid("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
-            build_grid("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"),
+            build_wire_grid("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+            build_wire_grid("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"),
         )
         == 135
     )
